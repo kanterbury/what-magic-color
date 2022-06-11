fce2bb3c9078d075944f01c279fb6fdbd8c17fc4
kanterbury <ksuzuki.db@gmail.com>
2022-06-11 14:16:26 +0900

    fix color order

--- src/CheckBoxes.svelte
+++ src/CheckBoxes.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import type { ManaColor } from './models/ManaColor';
   const colors: { label: string; id: ManaColor }[] = [
-    { label: '赤', id: 'red' },
-    { label: '黒', id: 'black' },
-    { label: '青', id: 'blue' },
     { label: '白', id: 'white' },
+    { label: '青', id: 'blue' },
+    { label: '黒', id: 'black' },
+    { label: '赤', id: 'red' },
     { label: '緑', id: 'green' },
   ];
   export let selected = [];
