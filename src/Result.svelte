69b160cfdb7f592117ba3cd0cc3c6089cf8ffed5
kanterbury <ksuzuki.db@gmail.com>
2022-06-11 13:45:28 +0900

    show test result

--- src/Result.svelte
+++ src/Result.svelte
@@ -2,6 +2,54 @@
 import type { ManaColor } from "./models/ManaColor";
 
   export let selectedColors: ManaColor[];
+  let magicColor: string;
+  
+  function getMagicNumber(selectedColors: ManaColor[]):number {
+    let magicNumber = 0;
+    if(selectedColors.includes("red")) {
+      magicNumber += 10000;
+    }
+    if(selectedColors.includes("black")) {
+      magicNumber += 1000;
+    }
+    if(selectedColors.includes("blue")) {
+      magicNumber += 100;
+    }
+    if(selectedColors.includes("white")) {
+      magicNumber += 10;
+    }
+    if(selectedColors.includes("green")) {
+      magicNumber += 1;
+    }
+
+    return magicNumber
+  }
+
+  function getMagicColor(magicNumber:number):string {
+    switch(magicNumber) {
+      case 10000: {
+        return "赤単";
+      }
+      case 1000: {
+        return "黒単"
+      }
+      case 100: {
+        return "青単"
+      }
+      case 10: {
+        return "白単"
+      }
+      case 1: {
+        return "緑単"
+      }
+      default: {
+        return "other"
+      }
+    }
+  }
+
+  $: magicNumber = getMagicNumber(selectedColors)
+  $: magicColor = getMagicColor(magicNumber)
 </script>
 
-<p>{selectedColors}</p>
+<p>MagicColor: {magicColor}</p>
