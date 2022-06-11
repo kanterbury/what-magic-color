0457473d657cf36c3d610b334e96d037eb6ed216
kanterbury <ksuzuki.db@gmail.com>
2022-06-11 14:00:40 +0900

    use prettier

--- src/CheckBoxes.svelte
+++ src/CheckBoxes.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-import type { ManaColor } from "./models/ManaColor";
- const colors: {label: string, id:ManaColor}[] = [{label: "赤", id: "red"}, {label: "黒", id: "black"}, {label: "青", id: "blue"}, {label: "白", id: "white"}, {label: "緑", id: "green"}];
-  export let selected = []
+  import type { ManaColor } from './models/ManaColor';
+  const colors: { label: string; id: ManaColor }[] = [
+    { label: '赤', id: 'red' },
+    { label: '黒', id: 'black' },
+    { label: '青', id: 'blue' },
+    { label: '白', id: 'white' },
+    { label: '緑', id: 'green' },
+  ];
+  export let selected = [];
 </script>
 
 {#each colors as color}
