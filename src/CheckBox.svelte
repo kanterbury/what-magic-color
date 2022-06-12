ee5ee445d6cddca69c450c829675eced80baf0ba
kanterbury <ksuzuki.db@gmail.com>
2022-06-12 22:12:29 +0900

    add CheckBox component and use iconify

---
+++ src/CheckBox.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte';
+  import type { ManaColor } from './models/ManaColor';
+  export let label: string;
+  export let id: ManaColor;
+  export let icon: string;
+  export let selected: ManaColor[];
+</script>
+
+<label>
+  <div class="icon-container" class:selected={selected.includes(id)}>
+    <Icon {icon} height={64} width={64} />
+  </div>
+  {label}
+  <input class="" type="checkbox" bind:group={selected} value={id} />
+</label>
+
+<style>
+  :nth-of-type(n + 2) {
+    margin-left: 16px;
+  }
+  .icon-container {
+    color: #aaa;
+    transition: color 0.2s;
+  }
+  .selected {
+    color: var(--color);
+  }
+</style>
--- src/CheckBoxes.svelte
+++ src/CheckBoxes.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
+  import Icon from '@iconify/svelte';
+  import CheckBox from './CheckBox.svelte';
   import type { ManaColor } from './models/ManaColor';
-  const colors: { label: string; id: ManaColor }[] = [
-    { label: '白', id: 'white' },
-    { label: '青', id: 'blue' },
-    { label: '黒', id: 'black' },
-    { label: '赤', id: 'red' },
-    { label: '緑', id: 'green' },
+  const colors: { label: string; id: ManaColor; icon: string }[] = [
+    { label: '白', id: 'white', icon: 'bi:sun-fill' },
+    { label: '青', id: 'blue', icon: 'entypo:drop' },
+    { label: '黒', id: 'black', icon: 'ion:skull-sharp' },
+    { label: '赤', id: 'red', icon: 'fontisto:fire' },
+    { label: '緑', id: 'green', icon: 'bi:tree-fill' },
   ];
-  export let selected = [];
+  export let selected: ManaColor[];
 </script>
 
-{#each colors as color}
-  <label>
-    {color.label}
-    <input type="checkbox" bind:group={selected} value={color.id} />
-  </label>
-{/each}
+<div class="container">
+  {#each colors as color}
+    <CheckBox
+      label={color.label}
+      id={color.id}
+      icon={color.icon}
+      --color={"#000"}
+      bind:selected
+    />
+  {/each}
+</div>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+  }
+</style>
