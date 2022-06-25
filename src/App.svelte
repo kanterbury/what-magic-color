ea29703bfd4b70df2769be67565dfb92714d5822
kanterbury <ksuzuki.db@gmail.com>
2022-06-25 14:54:01 +0900

    add footer

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
   let selectedColors: ManaColor[] = [];
 </script>
 
+<div id="container">
 <main>
   <Icon icon="arcticons:magic" height={128} width={128} color="#ff3e00" />
   <h1>What magic color?</h1>
@@ -16,6 +17,12 @@
   </div>
   
 </main>
+  <footer>
+    <p>本Webページはファンコンテンツ・ポリシーに沿った非公式のファンコンテンツです。ウィザーズ社の認可/許諾は得ていません。題材の一部に、ウィザーズ・オブ・ザ・コースト社の財産を含んでいます。©Wizards of the Coast LLC.</p>
+    <p>色の組み合わせについては、<a href="http://mtgwiki.com/wiki/%E8%89%B2%E3%81%AE%E7%B5%84%E3%81%BF%E5%90%88%E3%82%8F%E3%81%9B#3.E8.89.B2.E3.81.AE.E7.B5.84.E3.81.BF.E5.90.88.E3.82.8F.E3.81.9B">M:TG Wiki</a>を参考にさせていただきました。</p>
+  </footer>
+</div>
+
 
 <style>
   :global(body) {
@@ -32,6 +39,13 @@
     padding: 1em;
     margin: 0 auto;
   }
+  footer {
+    position: absolute;
+    text-align: center;
+    bottom: 0;
+    padding: 1em;
+    font-size: 12px;
+  }
 
   h1 {
     color: #ff3e00;
@@ -46,17 +60,11 @@
     margin: 1rem auto;
     line-height: 1.35;
   }
+  #container {
+    min-height: 100vh;
+    position: relative;
+  }
   #result-container {
     margin-top: 16px;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
   }
 </style>
