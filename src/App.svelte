12dc917c2416891c286571a09f4c3e4be8c6841e
kanterbury <ksuzuki.db@gmail.com>
2022-06-25 14:36:58 +0900

    reset body margin

--- src/App.svelte
+++ src/App.svelte
@@ -18,6 +18,10 @@
 </main>
 
 <style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
